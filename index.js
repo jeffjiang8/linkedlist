cbb85612dc83eba2df4f758e542d2492cc276f3a
Jeff Jiang <jeffjiang8@gmail.com>
2020-06-10 18:07:44 -0400

    insertFirst

--- index.js
+++ index.js
@@ -9,4 +9,15 @@ class LinkedList {
     constructor() {
         this.head = null
     }
+
+    insertFirst(data) {
+        // const node = new Node(data, this.head)
+        // //assign the current node to the next reference to the new node
+        // this.head = node
+        // //assgin the current node to the head of the linked list
+
+        //OR
+
+        return this.insertAt(data, 0)
+    }
 }
